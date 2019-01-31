7e26099e3658e430020ff5e23b95fd20ea1eb0ba
Brandon Tran <brandt2@uci.edu>
2019-01-31 13:50:09 -0800

    Add user Auth

--- Music_App/app/helpers/application_helper.rb
+++ Music_App/app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def auth_token
+    "<input
+    type='hidden'
+    name='authenticity_token'
+    value= '#{form_authenticity_token}'/>".html_safe
+  end
+
+  def logout
+    "<form action='#{session_url}' method='POST'>
+      <input type='hidden' name='_method' value='DELETE'>
+      #{auth_token}
+      <input type='submit' value='Logout' class='btn btn-danger'>
+    </form>".html_safe
+  end
+
 end
